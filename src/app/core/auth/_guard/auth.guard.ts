5774db448e47abd0145b60e1ea624efde91504fd
Megha Parmar <megha.d.parmar2018@gmail.com>
2023-05-15 19:37:40 +0530

    some chnage on auth-guard

--- src/app/core/auth/_guard/auth.guard.ts
+++ src/app/core/auth/_guard/auth.guard.ts
@@ -1,17 +1,18 @@
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { GlobalConstants } from '@common/GlobalConstants';
-import { AuthService } from '@service/auth.service';
+import { EncryptDecryptService } from '@service/encrypt-decrypt.service';
 
 
 export const AuthGuard = () => {
-  return inject(AuthService)
-    .getDecryptLocalStorageDetail(GlobalConstants.user) ? true : inject(Router).parseUrl('/auth/login');
+  return inject(EncryptDecryptService)
+    .getDecryptedLocalStorage(GlobalConstants.user) ? true : inject(Router).parseUrl('/auth/login');
+
 
 };
 
 export const LoginGuard = () => {
-  return !inject(AuthService)
-    .getDecryptLocalStorageDetail(GlobalConstants.user) ? true : inject(Router).parseUrl('/dashboard');
+  return !inject(EncryptDecryptService)
+    .getDecryptedLocalStorage(GlobalConstants.user) ? true : inject(Router).parseUrl('/dashboard');
 
 };
